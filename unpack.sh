5a80d962e89621d5d95fb3c1d6fac83c7da2a8b6
Serhii Veretennikov <gridis72@gmail.com>
2024-02-28 00:43:54 +0100

    feat: created new unpack script

---
+++ unpack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -d ~/.config/nvim ];
+then
+	cp -rf ~/.config/nvim ~/.config/nvim-old
+	echo "old nvim config was moved to ~/.config/nvim-old"
+fi
+
+cp -rf ./nvim ~/.config/nvim
+echo "nvim config unpacked"
+
+if [ -f ~/.tmux.conf ];
+then
+	cp -rf ~/.tmux.conf ~/.tmux.conf-old
+	echo "old tmux config was moved to ~/.tmux.conf-old"
+fi
+
+cp -rf ./.tmux.conf ~/.tmux.conf
+echo "tmux config unpacked"
+
+if [ -f ~/.zshrc ];
+then
+	cp -rf ~/.zshrc ~/.zshrc-old
+	echo "old zsh config was moved to ~/.zshrc-old"
+fi
+
+cp -rf ./.zshrc ~/.zshrc
+echo "tmux config unpacked"
