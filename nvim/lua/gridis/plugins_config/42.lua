be8d91983968b7cd2ea00f5d176e96a2bb5f002b
Serhii Veretennikov <gridis72@gmail.com>
2024-05-16 21:35:12 +0200

    feat: added remap and changed 42 header config

--- nvim/lua/gridis/plugins_config/42.lua
+++ nvim/lua/gridis/plugins_config/42.lua
@@ -1,4 +1,4 @@
 require("42header").setup({
-	user = "Gridis",
-	mail = "gridis72@gmail.com",
+	user = "svereten",
+	mail = "marvin@42.fr",
 })
--- nvim/lua/gridis/remaps.lua
+++ nvim/lua/gridis/remaps.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>nh", ":noh<Cr>")
