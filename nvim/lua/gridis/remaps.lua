40532ce1fffa8fc62a87cd519707b9dd3c7a6cf0
Serhii Veretennikov <gridis72@gmail.com>
2024-08-14 14:57:42 +0200

    feat: added line movement to nvim config

--- nvim/lua/gridis/remaps.lua
+++ nvim/lua/gridis/remaps.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<leader>nh", ":noh<Cr>")
+
+vim.keymap.set("v", "<C-j>", ":m '>+1<Cr>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<Cr>gv=gv")
